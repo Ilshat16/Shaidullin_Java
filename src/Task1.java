68677612ec332aa94fd7a518690eff2d1c56acf7
ilshat16 <illliat@mail.ru>
2023-10-04 17:00:34 +0300

    Add Task1

--- src/Task1.java
+++ src/Task1.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Task1 {
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int number = scanner.nextInt();
+        if (number > 7)
+            System.out.println("Привет");
+    }
 }
